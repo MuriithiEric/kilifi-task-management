f26c1e94303c0d1493ba6856cc0bafe9d598b55d
MuriithiEric <56841703+MuriithiEric@users.noreply.github.com>
2024-02-26 20:27:01 +0300

    setting up firebase

---
+++ auth.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCR4P76VK8MhBNr_USQp2JrDcY6PkLX6-0",
+  authDomain: "task-management-b9527.firebaseapp.com",
+  projectId: "task-management-b9527",
+  storageBucket: "task-management-b9527.appspot.com",
+  messagingSenderId: "927835389371",
+  appId: "1:927835389371:web:c6b8d86663bbf4b2ed989e",
+  measurementId: "G-22QTNHF006",
+};
